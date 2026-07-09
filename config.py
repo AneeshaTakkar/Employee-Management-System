ca2f87e0f9fd2be2bf0633fc27a250ccb05589be
Anika0307 <anikatakkar1910@gmail.com>
2026-07-09 12:47:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "5f6b14f3c6a74e9a8d2e814f71fbbc90")
     SESSION_TYPE = "filesystem"
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/ems_pro")
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://aneeshatakkar2403_db_user:MDu6labZEkX3qiNO@ems.dupmxw0.mongodb.net/ems_pro?appName=EMS")
